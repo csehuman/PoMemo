35f14a5d0b1a20a32e60c8a2c3b5eab39c15a474
csehuman <paullee20204@snu.ac.kr>
2022-08-25 13:27:59 +0700

    Initial Commit

---
+++ PoMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PoMemo
+//
+//  Created by Paul Lee on 2022/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
