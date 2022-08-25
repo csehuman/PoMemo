7a888c6584880a2bf0b5eef3c72c718169c430f4
csehuman <paullee20204@snu.ac.kr>
2022-08-25 14:14:19 +0700

    Step 5 - Build memo class

---
+++ PoMemo/Model/Model.swift
@@ -0,0 +1,24 @@
+//
+//  Model.swift
+//  PoMemo
+//
+//  Created by Paul Lee on 2022/08/25.
+//
+
+import Foundation
+
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    init(content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "Lorem Ipsum"),
+        Memo(content: "Subscribe Please🙏"),
+        Memo(content: "How is your day?🔥")
+    ]
+}
