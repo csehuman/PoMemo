a1fd9b8c60e4f573482779847ec5826754f34a35
csehuman <paullee20204@snu.ac.kr>
2022-08-26 10:14:12 +0700

    Step 10 - Implement save action

--- PoMemo/ComposeViewController.swift
+++ PoMemo/ComposeViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ComposeViewController: UIViewController {
     
+    @IBOutlet weak var memoTextView: UITextView!
+    
     @IBAction func close(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
-
+    @IBAction func save(_ sender: Any) {
+        guard let memo = memoTextView.text, memo.count > 0 else {
+            alert(message: "메모를 입력하세요")
+            return
+        }
+        
+        let newMemo = Memo(content: memo)
+        Memo.dummyMemoList.append(newMemo)
+        
+        NotificationCenter.default.post(name: ComposeViewController.newMemoDidInsert, object: nil)
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,4 +46,9 @@ class ComposeViewController: UIViewController {
     }
     */
 
+}
+
+
+extension ComposeViewController {
+    static let newMemoDidInsert = Notification.Name(rawValue: "newMemoDidInsert")
 }
